348f607191b6ae87569dca337d58f03937acaeb8
Ruth Williams <rw269[at]yahoo.com>
2013-01-31 18:18:22 +0000

    Now with four tests

--- TestContact.java
+++ TestContact.java
@@ -1,7 +1,7 @@
 /**
 * Test for the ContactImpl implimentation of the Contact interface
-* 30th January 2013 - Created with all tests set to fail
-* Compiles and then fails all four tests
+* 30th January 2013 - Created with four dummy tests - all set to fail
+* 31st January 2013 - Real tests
 */
 
 import org.junit.*;
@@ -9,29 +9,61 @@ import static org.junit.Assert.*;
 
 public class TestContact
 {
+	private ContactImpl c1;
+	private ContactImpl c2;
+
+	@Before
+	public void buildContact()
+	{
+		c1 = new ContactImpl ("Smith, John", "Not the deceased Labour politican");
+		c2 = new ContactImpl ("Jones, Jack", "");
+	}
 
 	@Test
-	public void testsGetID()
+	public void testsGetId()
 	{
-		fail("Test not yet implemented");
+		int output = c1.getId();
+		int expected = 1001;
+		assertEquals(output, expected);
+		int output2 = c2.getId();
+		int expected2 = 1002;
+		assertEquals(output2, expected2);
 	}
 
 	@Test
 	public void testsGetName()
 	{
-		fail("Test not yet implemented");
-	}
-
-	@Test
-	public void testsAddNotes()
-	{
-		fail("Test not yet implemented");
+		String output = c1.getName();
+		String expected = "Smith, John";
+		assertEquals(output, expected);
+		String output2 = c2.getName();
+		String expected2 = "Jones, Jack";
+		assertEquals(output2, expected2);
 	}
 
 	@Test
 	public void testsGetNotes()
 	{
-		fail("Test not yet implemented");
+		String output = c1.getNotes();
+		String expected = "Not the deceased Labour politican";
+		assertEquals(output, expected);
+		String output2 = c2.getNotes();
+		String expected2 = "";
+		assertEquals(output2, expected2);
+
+	}
+
+	@Test
+	public void testsAddNotes()
+	{
+		c1.addNotes("More data");
+		String output = c1.getNotes();
+		String expected = "Not the deceased Labour politicanMore data";
+		assertEquals(output, expected);
+		c2.addNotes("Notes about Jack");
+		String output2 = c2.getNotes();
+		String expected2 = "Notes about Jack";
+		assertEquals(output2, expected2);
 	}
 
 }
