34b15c00f1a6eb991942acbd0ea7bf9e29f821c5
Ruth Williams <rw269[at]yahoo.com>
2013-01-31 18:02:07 +0000

    The list of tests without any implementation

---
+++ TestContact.java
@@ -0,0 +1,37 @@
+/**
+* Test for the ContactImpl implimentation of the Contact interface
+* 30th January 2013 - Created with all tests set to fail
+* Compiles and then fails all four tests
+*/
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TestContact
+{
+
+	@Test
+	public void testsGetID()
+	{
+		fail("Test not yet implemented");
+	}
+
+	@Test
+	public void testsGetName()
+	{
+		fail("Test not yet implemented");
+	}
+
+	@Test
+	public void testsAddNotes()
+	{
+		fail("Test not yet implemented");
+	}
+
+	@Test
+	public void testsGetNotes()
+	{
+		fail("Test not yet implemented");
+	}
+
+}
